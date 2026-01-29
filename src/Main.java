14bb13ec6cdc74c7652b6911772e44f9eb381ef5
daud0095 <daudibragimov2004@gmail.com>
2026-01-29 16:27:38 +0100

    First task

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-public class Main {
+double item1 = 25.50;
+double item2 = 15.00;
+double item3 = 30.00;
+double TaxRate = 25.0;
+
+
+void main() {
+    double subtotal = item1 + item2 + item3;
+    double TaxAmount = subtotal + (subtotal * TaxRate / 100.0);
+
+    System.out.println(subtotal);
+    System.out.println(TaxAmount);
 }
