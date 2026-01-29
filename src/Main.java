38dbc6044b7c358d9ff6c02e485af7c38c0e1485
daud0095 <daudibragimov2004@gmail.com>
2026-01-29 18:21:50 +0100

    All taskes 1 - 15

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+/*
 double item1 = 25.50;
 double item2 = 15.00;
 double item3 = 30.00;
@@ -11,3 +12,192 @@ void main() {
     System.out.println(subtotal);
     System.out.println(TaxAmount);
 }
+*/
+
+/*
+// Opgave 2
+double food = 450.0;
+double drinks = 120.0;
+double drinkMoney = 50.0;
+int people = 4;
+double totalPrice;
+double pricePerPerson;
+
+void main() {
+
+    totalPrice = food + drinks + drinkMoney;
+    pricePerPerson = totalPrice / people;
+
+    System.out.println(pricePerPerson);
+}
+ */
+/*
+// Opgave 3
+double originalPrice = 300.0;
+double rabetProcent = 20;
+double discount;
+double momsProcent = 25.0; 
+
+void main() {
+    System.out.println(originalPrice);
+    discount = originalPrice - (originalPrice * rabetProcent / 100.0);
+    System.out.println(discount);
+    momsProcent = discount + (discount * momsProcent / 100.0);
+    System.out.println(momsProcent);
+}
+ */
+/*
+// Opgave 4
+int guests = 15;
+int cupcakes = 40;
+int cupcakesPerGuest;
+int cupcakesLeft;
+
+void main() {
+    cupcakesPerGuest = cupcakes / guests;
+
+    cupcakesLeft = cupcakes % guests;
+
+    System.out.println("Each guest gets " + cupcakesPerGuest + " cupcakes left over.");
+    System.out.println(cupcakesLeft);
+}
+ */
+
+/*
+// Opgave 5
+String name = "Anna";
+int age = 25;
+
+void main(){
+   StringBuilder sb = new StringBuilder();
+   sb.append("Hello, ");
+   sb.append(name);
+   sb.append("! ");
+   sb.append("You are ");
+   sb.append(age);
+   sb.append(" years old.");
+
+   System.out.println(sb.toString());
+}
+ */
+
+/*
+// Opgave 6
+int score = 100;
+
+void main(){
+    System.out.println(score);
+    score += 50;
+    System.out.println(score);
+    score += 25;
+    System.out.println(score);
+}
+ */
+
+/*
+// Opgave 7
+int lifeHealth = 100;
+
+void main(){
+    lifeHealth -= 15;
+    System.out.println(lifeHealth);
+    lifeHealth -= 20;
+    System.out.println(lifeHealth);
+}
+ */
+
+/*
+// Opgave 8
+double price = 50.0; 
+
+void main() {
+    System.out.println(price);
+    price *= 2;
+    System.out.println(price);
+}
+ */
+
+/*
+// Opgave 9
+int count = 10;
+
+void main(){
+    System.out.println(count);
+    count++;
+    System.out.println(count);
+    count--;
+    System.out.println(count);
+}
+ */
+
+/*
+// Opgave 10
+int number = 10;
+
+void main() {
+    number += 5;
+    System.out.println(number);
+    number -= 3;
+    System.out.println(number);
+    number++;
+    System.out.println(number);
+    number--;
+    System.out.println(number);
+}
+ */
+/*
+// Opgave 11
+String laptop = "Laptop";
+double productPrice = 7999.99;
+boolean isAvailable = true;
+int productLeft = 15;
+double rabatProcent = 10.0;
+String category = "Electronics";
+ */
+
+/*
+// Opgave 12
+String studentName = "Anna";
+int testPoint = 85;
+int pointAflevering = 92;
+double gradeAverage = 88.5;
+boolean CoursePassed = true;
+ */
+
+/*
+// Opgave 13
+String name = "Peter Hansen";
+int guestroomNumber = 204;
+int guestNights = 3;
+double overNightPrice = 899.00;
+boolean isBreakfastIncluded = true;
+double totalPriceForGuest = 2697.00;
+ */
+
+/*
+// Opgave 14
+int stepsTakenToday = 8547;
+int stepsGoal = 10000;
+double stepsTakenMiles = 6.2;
+int caloriesBurned = 420;
+boolean reachedGoal = false;
+ */
+
+/*
+// Opgave 15
+String bookTitle = "RICH DAD POOR DAD";
+int isbnNumber = 38838493992;
+int bookPages = 300;
+boolean isBarrow = true;
+String currentHolder = "Mark Jensen";
+String dueDate = "30-03-2026";
+double feePrice = 20;
+boolean isBestseller = true;
+double bookRating = 4.7; 
+ */
+
+
+
+
+
+
